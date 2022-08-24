f3619ef009a221891032fdc6838f8d8251afa110
davispacheco <davipacheco1996@gmail.com>
2022-08-24 18:49:25 -0300

    Adicionar, na interface ContasAPagarRepository, os métodos findByStatus e findByTipo.

--- src/main/java/com/modulo5/catalisa4desafio3/repository/ContasAPagarRepository.java
+++ src/main/java/com/modulo5/catalisa4desafio3/repository/ContasAPagarRepository.java
@@ -1,9 +1,16 @@
 package com.modulo5.catalisa4desafio3.repository;
 
 import com.modulo5.catalisa4desafio3.model.ContasAPagarModel;
+import com.modulo5.catalisa4desafio3.model.enums.Status;
+import com.modulo5.catalisa4desafio3.model.enums.Tipo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContasAPagarRepository extends JpaRepository<ContasAPagarModel, Long> {
+    List<ContasAPagarModel> findByStatus(Status status);
+    List<ContasAPagarModel> findByTipo(Tipo tipo);
+    }
 }

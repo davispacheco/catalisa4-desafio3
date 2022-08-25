4b6e5b7a7863dc9820c86a90c625761950569512
davispacheco <davipacheco1996@gmail.com>
2022-08-25 18:03:50 -0300

    Implementar classe para tratamento de exceções.

--- src/main/java/com/modulo5/catalisa4desafio3/exception/ExceptionHandlerContasAPagar.java
+++ src/main/java/com/modulo5/catalisa4desafio3/exception/ExceptionHandlerContasAPagar.java
@@ -1,4 +1,38 @@
 package com.modulo5.catalisa4desafio3.exception;
 
-public class ExceptionHandlerContasAPagar {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerContasAPagar extends ResponseEntityExceptionHandler {
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String mensagemUser = messageSource.getMessage("mensagem.invalida", null, null);
+        String mensagemDev = ex.getCause().toString();
+        return handleExceptionInternal(ex, new MensagemErro(mensagemUser, mensagemDev), headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MensagemErro {
+
+        private String mensagemDoUsuario;
+        private String mensagemDoDev;
+    }
 }
